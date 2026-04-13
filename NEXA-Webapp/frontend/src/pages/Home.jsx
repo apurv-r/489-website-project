629ecc137bcd6430e92726a970df6f21eca566b5
Apurv Rane <apurv.r11@gmail.com>
2026-04-12 17:31:38 -0700

    eslint fix

--- NEXA-Webapp/frontend/src/pages/Home.jsx
+++ NEXA-Webapp/frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 
 export default function Home() {
   const navigate = useNavigate();
